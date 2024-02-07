e323b3245b2ebe59978415580180999f21941d61
marlinverhulst <marlinverhulst@gmail.com>
2024-02-07 18:19:46 +0100

    Corrected the observer example

--- ColorRect.gd
+++ ColorRect.gd
@@ -2,6 +2,21 @@ extends ColorRect
 
 # Called when the node enters the scene tree for the first time.
 @onready var callBack = Callable(self, "makeInvisable" )
+var subscriptionId
+func _ready() -> void :
+	pass
 
-func makeInvisable(b: bool):
+func _process(delta) -> void:
+	subsribeToBackspace()
+	if Input.is_action_just_pressed("ui_down"):
+		get_parent().backspaceSubject.unsubscribe(subscriptionId)
+	
+func makeInvisable(b: bool) -> void:
 	visible = b 
+
+func subsribeToBackspace() -> void:
+	if !subscriptionId:
+		print("Trying")
+		if get_parent().is_node_ready(): 
+			print("subscribing")
+			subscriptionId = get_parent().backspaceSubject.subscribe(callBack)
--- main.gd
+++ main.gd
@@ -3,13 +3,12 @@ extends Node2D
 var level1Path = "res://Levels/level_1.tscn"
 var loadPath = "res://Splash/loading_screen.tscn"
 
-@onready var backspaceSubject = Subject.new()
+@onready var backspaceSubject: Subject = Subject.new()
 var bools: bool = false
-var subscriptionId: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	subscriptionId = backspaceSubject.subscribe($ColorRect.callBack)
+	pass
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,8 +18,7 @@ func _process(_delta) -> void:
 	if Input.is_action_just_pressed("ui_text_backspace"):
 		backspaceSubject.emit(bools)
 		bools = !bools
-	if Input.is_action_just_pressed("ui_down"):
-		backspaceSubject.unsubscribe(subscriptionId)
+
 		
 		
 
