6337aac20bb01d038238c5a7a6da3f83b99939c7
marlinverhulst <marlinverhulst@gmail.com>
2024-02-07 21:14:12 +0100

    spiced up the loading screen a little bit

--- Levels/Game_Started.gd
+++ Levels/Game_Started.gd
@@ -1,4 +1,13 @@
 extends State
 
+@onready var playfieldNode = $"../../playfield"
+
 func stateEntered(): 
 	print('entered GAME_STARTED')
+	var playerScene = load("res://assets/yellow_player/yellow_player.tscn")
+	var player = playerScene.instantiate()
+	player.position.x = 240
+	player.position.y = 201
+	player.set_rotation_degrees(90)
+	playfieldNode.add_child(player)
+
