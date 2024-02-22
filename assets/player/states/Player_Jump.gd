a21e3694e09db9511e9ebe05e10a0331cdcf8c29
marlinverhulst <marlinverhulst@gmail.com>
2024-02-22 22:03:12 +0100

    fixed jump

--- assets/player/states/Player_Jump.gd
+++ assets/player/states/Player_Jump.gd
@@ -7,12 +7,16 @@ func stateEntered():
 	print('Jump entered')
 	sprite.play("walk")
 	
+func stateExit(_nextState: String):
+	jumped = false
+	
 func statePhysicsProcess(delta: float) -> void :
 	if not jumped:
 		character.velocity.y += host.JUMP_VELOCITY
 		jumped = true
+		sprite.play("walk")
 	if not character.is_on_floor():
-		character.velocity.y += host.gravity * delta
+		character.velocity.y += host.gravity * delta	
 		
 	var direction = Input.get_axis("ui_left", "ui_right")	
 	if direction:
@@ -22,13 +26,10 @@ func statePhysicsProcess(delta: float) -> void :
 	else:
 		character.velocity.x = move_toward(character.velocity.x, 0, host.SPEED)
 		sprite.play("walk") # but should be falling
-	if character.is_on_floor():
-		jumped = false
-		host.changeState('Walk')	
+		
 	character.move_and_slide()
 
-func stateUnhandledInput(event) -> void : 
-	pass
-	
+	if character.is_on_floor():
+		host.changeState("Walk")
 	
 	
